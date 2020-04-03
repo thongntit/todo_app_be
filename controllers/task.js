4e39783c283bdffb764b46994b05f1c216d576bf
Kajekk <tangquocvinh1000@gmail.com>
2020-04-03 17:26:40 +0700

    API Get todo by ID

--- controllers/task.js
+++ controllers/task.js
@@ -3,7 +3,7 @@ const Task = require('../models').Task;
 exports.createTask = (req, res) => {
   if (!req.body.title) {
     res.status(400).send({
-      message: "Title can not be empty!"
+      message: 'Title can not be empty!',
     });
     return;
   }
@@ -18,8 +18,26 @@ exports.createTask = (req, res) => {
 
   Task.create(task)
     .then(task => res.status(201).send(task))
-    .catch(error => res.status(500).send({
-      message:
-        err.message || "Some error occurred while creating task."
-    }));
+    .catch(error =>
+      res.status(500).send({
+        message: error.message || 'Some error occurred while creating task.',
+      }),
+    );
+};
+
+exports.findTaskById = (req, res) => {
+  if (!req.body.id) {
+    res.status(400).send({
+      message: 'Id can not be empty!',
+    });
+    return;
+  }
+
+  Task.findByPk(req.body.id)
+    .then(task => res.status(200).send(task))
+    .catch(error =>
+      res.status(500).send({
+        message: error.message || 'Some error occurred while getting task by id.',
+      }),
+    );
 };
