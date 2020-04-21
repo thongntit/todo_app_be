dd0289e5fd1e96f49177911356978f6eb3764349
kajekk <tqvinh@tma.com.vn>
2020-04-21 23:27:13 +0700

    Update cors

--- app.js
+++ app.js
@@ -1,22 +1,13 @@
 const express = require("express");
 const app = express();
 const port = process.env.PORT || 3000;
+const cors = require("cors");
 const bodyParser = require("body-parser");
 const router = require("./router");
-const session = require("express-session");
-const FileStore = require("session-file-store")(session);
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
-
-app.use(
-  session({
-    secret: process.env.session_secret || "secret",
-    saveUninitialized: false,
-    resave: false,
-    store: new FileStore(),
-  })
-);
+app.use(cors());
 app.use(router);
 
 app.listen(port, () => console.log(`Example app listening on port ${port}!`));
