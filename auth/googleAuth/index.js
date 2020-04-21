6d8891e62be2e4d5b2236b604a02c8c9fbe279e4
kajekk <tqvinh@tma.com.vn>
2020-04-21 23:39:33 +0700

    fix data validation

--- auth/googleAuth/index.js
+++ auth/googleAuth/index.js
@@ -9,49 +9,58 @@ exports.login = async (req, res) => {
     });
   } else {
     let data = jwt.decode(tokenId);
-    try {
-      User.findOne({
-        where: {
-          email: data.email,
-        },
-      })
-        .then((foundUser) => {
-          if (!foundUser) {
-            User.create({
-              username: data.name,
-              email: data.email,
-              authenticationString: tokenId,
-            })
-              .then((user) => {
-                res.status(201).send(user);
-              })
-              .catch((error) =>
-                res.status(500).send({
-                  message:
-                    error.message || "Some error occurred while creating user",
-                })
-              );
-          } else {
-            foundUser
-              .update({ authenticationString: tokenId })
-              .then((user) => {
-                res.status(200).send(foundUser);
-              })
-              .catch((error) =>
-                res.status(500).send({
-                  message:
-                    error.message || "Some error occurred while updating user",
-                })
-              );
-          }
+    if (!data) {
+      res.status(400).send({
+        message: "invalid tokenId",
+      });
+    } else {
+      try {
+        User.findOne({
+          where: {
+            email: data.email,
+          },
         })
-        .catch((error) =>
-          res.status(500).send({
-            message: error.message || "Some error occurred while finding user",
+          .then((foundUser) => {
+            if (!foundUser) {
+              User.create({
+                username: data.name,
+                email: data.email,
+                authenticationString: tokenId,
+              })
+                .then((user) => {
+                  res.status(201).send(user);
+                })
+                .catch((error) =>
+                  res.status(500).send({
+                    message:
+                      error.message ||
+                      "Some error occurred while creating user",
+                  })
+                );
+            } else {
+              foundUser
+                .update({ authenticationString: tokenId })
+                .then((user) => {
+                  res.status(200).send(foundUser);
+                })
+                .catch((error) =>
+                  res.status(500).send({
+                    message:
+                      error.message ||
+                      "Some error occurred while updating user",
+                  })
+                );
+            }
           })
-        );
-    } catch (error) {
-      res.status(500).send(error);
+          .catch((error) =>
+            res.status(500).send({
+              message:
+                error.message || "Some error occurred while finding user",
+            })
+          );
+      } catch (error) {
+        res.status(500).send(error);
+      }
     }
   }
 };
