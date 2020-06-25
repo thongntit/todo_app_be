15806130096ef34025b1ee24d6f9ced53d205160
Thong Nguyen <thong.nt@teko.vn>
2020-06-25 13:06:59 +0700

    Update host

--- router/index.js
+++ router/index.js
@@ -2,8 +2,8 @@ const app = (module.exports = require("express")());
 
 ("use strict");
 
-app.use("/auth", require("./auth"));
-app.use("/tasks", require("./tasks"));
+app.use("/todo/auth", require("./auth"));
+app.use("/todo/tasks", require("./tasks"));
 
 app.all("*", (req, res) => {
   res.status(404).send({ msg: "not found" });
